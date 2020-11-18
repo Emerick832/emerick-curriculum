4aaf7d1a86731426cee6454860e77d7c78339d61
Emerick832 <gulhermeemerick@gmail.com>
2020-11-18 01:29:39 -0300

    pagina basica de sobre-nos

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,5 +1,57 @@
 export default function Sobre() {
     return(
-        <h1>#</h1>
+        <>
+        <div className="container">
+            <div className="quem__somos">  
+                <img className="quem__somos-imagem" src="/example-people.jpg" alt="Guilherme Foto"></img>              
+                <h2 className="nome">Guilherme Emerick</h2>
+                <p className="descricao">Graduado em Analise e Desenvolvimento de Sistemas com experiencia de 1 ano pela fabrica de software e 1 ano de estagio no Conselho Nacional de Justica.
+                </p>
+            </div>
+        </div>
+
+        <div className="container">
+            <div className="quem__somos">
+                <img className="quem__somos-imagem" src="/example-people.jpg" alt="Charles Foto"></img>              
+                <h2 className="nome">Charles Robson</h2>
+                <p className="descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam commodo ligula sed lacus suscipit molestie.
+                </p>
+            </div>
+        </div>
+
+        <style jsx>
+            {`
+            .container{
+                display: flex;
+                justify-content: center;
+            }
+            
+            .quem__somos{
+                display: flex;
+                flex-direction:column;
+                padding: 1.5rem;
+                width: 400px;
+                margin: 1rem;
+                text-align: center;
+                border: 1px solid #eaeaea;
+                border-radius: 10px;
+                transition: color 0.15s ease, border-color 0.15s ease;
+            }
+            
+            .quem__somos-imagem{
+                justify-content: center;
+                margin-left: 35%;
+                width: 120px;
+                height: 120px;
+                border-radius: 50%;            
+                align-items: center;    
+            }
+
+            
+            `
+        }            
+        </style>
+
+        </>
     )
 }
