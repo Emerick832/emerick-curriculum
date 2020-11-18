0c561b210ca34f6a1003f9c80efeef462975a91a
Emerick832 <gulhermeemerick@gmail.com>
2020-11-18 14:10:40 -0300

    pequenos ajustes

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,86 +1,94 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Sobre() {
-    return(
-        <>
-        <div className="container">
-            <div className="quem__somos">  
-                <img className="quem__somos-imagem" src="/example-people.jpg" alt="Guilherme Foto"></img>              
-                <h2 className="nome">Guilherme Emerick</h2>
-                <p className="descricao">Graduado em Analise e Desenvolvimento de Sistemas com experiencia de 1 ano pela fabrica de software e 1 ano de estagio no Conselho Nacional de Justica.
-                </p>
-            </div>
+  return (
+    <>
+      <div className="container">
+        <div className="quem__somos">
+          <img
+            className="quem__somos-imagem"
+            src="/example-people.jpg"
+            alt="Guilherme Foto"
+          ></img>
+          <h2 className="nome">Guilherme Emerick</h2>
+          <p className="descricao">
+            Graduado em Analise e Desenvolvimento de Sistemas com experiencia de
+            1 ano pela fabrica de software e 1 ano de estagio no Conselho
+            Nacional de Justica.
+          </p>
         </div>
+      </div>
 
-        <div className="container">
-            <div className="quem__somos">
-                <img className="quem__somos-imagem" src="/example-people.jpg" alt="Charles Foto"></img>              
-                <h2 className="nome">Charles Robson</h2>
-                <p className="descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam commodo ligula sed lacus suscipit molestie.
-                </p>
-            </div>    
+      <div className="container">
+        <div className="quem__somos">
+          <img
+            className="quem__somos-imagem"
+            src="/example-people.jpg"
+            alt="Charles Foto"
+          ></img>
+          <h2 className="nome">Charles Robson</h2>
+          <p className="descricao">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam commodo
+            ligula sed lacus suscipit molestie.
+          </p>
         </div>
+      </div>
 
-        <div className="container">
-                <Link href="/">
-                    <input className="voltar" type="button" value="Voltar"></input>
-                </Link>
-        </div>    
+      <div className="container">
+        <Link href="/">
+          <input className="voltar" type="button" value="Voltar"></input>
+        </Link>
+      </div>
 
+      <style jsx>
+        {`
+          .body {
+            height: auto;
+          }
 
-        <style jsx>
-            {`
+          .container {
+            box-sizing: border-box;
+            display: flex;
+            justify-content: center;
+          }
 
-            .body{
-                height: auto;
-            }
-            
-            .container{
-                box-sizing: border-box;
-                display: flex;
-                justify-content: center;
-            }
-            
-            .quem__somos{
-                display: flex;
-                flex-direction:column;
-                padding: 1.5rem;
-                width: 400px;
-                margin: 1rem;
-                text-align: center;
-                border: 1px solid #eaeaea;
-                border-radius: 10px;
-                transition: color 0.15s ease, border-color 0.15s ease;
-            }
-            
-            .quem__somos-imagem{
-                margin-left: auto;
-                margin-right: auto;
-                position:relative;
-                justify-content: center;
-                width: 120px;
-                height: 120px;
-                border-radius: 50%;            
-            }
+          .quem__somos {
+            display: flex;
+            flex-direction: column;
+            padding: 1.5rem;
+            width: 400px;
+            margin: 1rem;
+            text-align: center;
+            border: 1px solid #eaeaea;
+            border-radius: 10px;
+            transition: color 0.15s ease, border-color 0.15s ease;
+          }
 
-            .voltar{
-                margin: 1rem;
-                background: #2ea44f;
-                border: none;
-                cursor: pointer;
-                font-size: 18px;
-                height: 2rem;
-                justify-content: center;
-                text-decoration: none;
-                text-transform: uppercase;
-                width: 5rem;
-            }
+          .quem__somos-imagem {
+            margin-left: auto;
+            margin-right: auto;
+            position: relative;
+            justify-content: center;
+            width: 120px;
+            height: 120px;
+            border-radius: 50%;
+          }
 
-            
-            `
-        }            
-        </style>
+          .voltar {
+            margin: 1rem;
+            background: #2ea44f;
+            border: none;
+            cursor: pointer;
+            font-size: 18px;
+            height: 2rem;
+            justify-content: center;
+            text-decoration: none;
+            text-transform: uppercase;
+            width: 5rem;
+          }
 
-        </>
-    )
+        `}
+      </style>
+    </>
+  );
 }
