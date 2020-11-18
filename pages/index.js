b5510c8809386fd4ff78d516b49509d3e71800fe
Emerick832 <gulhermeemerick@gmail.com>
2020-11-18 11:51:34 -0300

    formatando index

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
@@ -20,7 +20,9 @@ export default function Home() {
         <div className="grid">
           <a href="sobre" className="card">
             <h3>Sobre Nós &rarr;</h3>
-            <p>Clique aqui para conhecer melhor os desenvolvedores da aplicação.</p>
+            <p>
+              Clique aqui para conhecer melhor os desenvolvedores da aplicação.
+            </p>
           </a>
 
           <a href="#" className="card">
@@ -28,9 +30,7 @@ export default function Home() {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="#"
-            className="card">
+          <a href="#" className="card">
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
@@ -53,7 +53,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
@@ -204,5 +204,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
