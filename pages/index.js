2a0b8e9d70f741fd0bf2fe6e2b07cf823f5e4738
Guilherme Emerick <42480909+Emerick832@users.noreply.github.com>
2021-04-14 16:16:12 -0300

    Adicionando Hotjar

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,18 @@ export default function Home() {
       <Head>
         <title>Portifólio</title>
         <link rel="icon" href="/favicon.ico" />
+    <!-- Hotjar Tracking Code for https://emerick-curriculum.vercel.app/ -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2350342,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+
       </Head>
 
       <main>
