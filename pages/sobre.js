79d24f2ecf2a48a46cafcce02d316bf2ac8b507a
Guilherme Emerick <42480909+Emerick832@users.noreply.github.com>
2021-04-14 16:16:40 -0300

    Adicionando Hotjar

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,4 +1,17 @@
 import Link from "next/link";
+<head>
+  <!-- Hotjar Tracking Code for https://emerick-curriculum.vercel.app/ -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2350342,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+</head>
 
 export default function Sobre() {
   return (
